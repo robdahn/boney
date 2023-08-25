a987aee96f22c15534282198318159ff3a626f43
Robert <52195853+robdahn@users.noreply.github.com>
2023-08-25 23:02:13 +0200

    Cleanup
    Removed old test cases with 7 class TPM with two bone subclasses (cortical and marrow).

--- boney_segment_simpleBone.m
+++ boney_segment_simpleBone.m
@@ -11,8 +11,6 @@ function out = boney_segment_simpleBone(seg8t,Ym,Yp)
 %  Ym         .. intensity normalized image
 %  Yp         .. SPM tissue classes
 %  out        .. output structure with basic information
-%   .spm_con  .. 
-%   .spm_bone_med# .. bone values just given by SPM tissue peaks 
 %   .int_ci   .. median tissue intensity of each tissue class
 %   .vol_ci   .. volume of each tissue class
 %   .TIV      .. total intracranial volume
@@ -23,42 +21,13 @@ function out = boney_segment_simpleBone(seg8t,Ym,Yp)
 %
 % _________________________________________________________________________
 %
-% Robert Dahnke
+% Robert Dahnke & Polona Kalc
 % Structural Brain Mapping Group (https://neuro-jena.github.io)
 % Departments of Neurology and Psychiatry
 % Jena University Hospital
 % _________________________________________________________________________
 
 
-
-  % == (1) SPM gaussians per tissue class ==
-  % get high prob value of CSF tissue
-  out.spm_con = sum(seg8t.mn( seg8t.lkp == 3 )' .* ...
-    ( seg8t.mg( seg8t.lkp == 3 ) == max(seg8t.mg( seg8t.lkp == 3 ) ) ));
-  
-  if max(seg8t.lkp) > 6
-    % RD202308: two-class bone tissue
-    % This was a special test case with artificial seperation of the bone
-    % tissue class into inner bonemarrow and outer hard bone. However, I am
-    % not sure if the classes are correctly used in following functions,  
-    % i.e. this case should maybe removed!
-    cat_io_cprintf('warn','\nWarning: a-typical TPM test-case that should have a soft and a hard bone class!\n')
-    out.spm_bone_med1 = seg8t.mn( seg8t.lkp == 4 ) / out.spm_con;
-    out.spm_bone_med2 = seg8t.mn( seg8t.lkp == 5 ) / out.spm_con;
-  else
-    % default case for 6 TPM classes and typical/various SPM subtissues 
-    % (i.e., [ 1 1 2 3 4 2 ]) with the typical 3 peaks for bone
-    bone = sort(seg8t.mn( seg8t.lkp == 4 ) / out.spm_con); % order intensities within class 4 (normalized by CSF)
-    out.spm_bone_med1 = bone(min(numel(bone),3)); % highest value (i.e. bone marrow) 
-    out.spm_bone_med2 = bone(1);                  % lowest values (i.e. hard bone) 
-    out.spm_bone_med3 = bone(min(numel(bone),2)); % second median value if available otherwise highest value
-    clear bone
-  end
- 
-
-
-  
-  % == (2) median bone intensity ==
   % load SPM tissue segments
   for ci = 1:(max(seg8t.lkp) - 1) 
      out.int_ci(1,ci) = median(Ym(Yp{ci}>0.9));     % intensity
