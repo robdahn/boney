1e6e5dc7e708aa839b44ea1a15847041c87ad02a
Robert <52195853+robdahn@users.noreply.github.com>
2023-08-25 23:02:45 +0200

    Create boney_segment_cleanup.m
    New function to remove temporary files.

---
+++ boney_segment_cleanup.m
@@ -0,0 +1,48 @@
+function Pout = boney_segment_cleanup(Pout,out,job,i)
+%boney_segment_cleanup. Update Pout structure and clean up files  
+
+
+  % xml file and report
+  Pout.xml{i}            = out(i).P.xml;
+  Pout.reports{i}        = out(i).P.report; 
+
+  if job.output.writeseg == 1
+    for ci = 1:5
+      Pout.cls{ci}{i}    = out(i).P.cls{ci}; 
+    end
+  elseif 0
+    % delete preprocessing segments
+    for ci = 1:5
+      if exist( out(i).P.cls{ci} , 'file')
+        delete( out(i).P.cls{ci} );
+      end
+    end
+  end
+
+
+  % boney volume files
+  if job.output.writevol
+    Pout.rbone_affine{i} = out(i).P.bonemarrow_affine; 
+    Pout.rbone{i}        = out(i).P.bonemarrow;
+  else
+    % delete files
+  end
+
+
+  % boney surface files
+  if job.output.writesurf
+    Pout.bcentral{i}     = out(i).P.central;  
+    Pout.bcortex{i}      = out(i).P.cortex;
+    Pout.bmarrow{i}      = out(i).P.marrow;
+    Pout.bthickness{i}   = out(i).P.thick; 
+    Pout.hthickness{i}   = out(i).P.headthick; 
+  elseif job.opts.bmethod == 2 % surface processing
+    FD = {'central','cortex','marrow','thick','headthick'}; 
+    for fdi = 1:numel(FD)
+      if exist( out(i).P.(FD{fdi}) , 'file')
+        delete( out(i).P.(FD{fdi}) );
+      end
+    end
+  end
+
+end
