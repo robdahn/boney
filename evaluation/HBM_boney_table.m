896e2e3c519382b60542127f99b895eb841bdce8
Robert <52195853+robdahn@users.noreply.github.com>
2023-11-30 21:02:35 +0100

    Updated HBM figures

--- evaluation/HBM_boney_table.m
+++ evaluation/HBM_boney_table.m
@@ -8,7 +8,7 @@
 % _________________________________________________________________________
 
 
-fontsize     = 15;
+fontsize     = 14;
 boneyEvalDir = fileparts(which('HBM_boney_table.m'));
 
 % load data
@@ -16,23 +16,23 @@ men_corr   = cat_io_csv(fullfile(boneyEvalDir,'HBM_2024','men_corr_Boney.csv'));
 men_pval   = cat_io_csv(fullfile(boneyEvalDir,'HBM_2024','men_pval_Boney.csv'));
 women_corr = cat_io_csv(fullfile(boneyEvalDir,'HBM_2024','women_corr_Boney.csv'));
 women_pval = cat_io_csv(fullfile(boneyEvalDir,'HBM_2024','women_pval_Boney.csv'));
-%both_corr  = cat_io_csv(fullfile(boneyEvalDir,'HBM_2024','both_corr_Boney.csv'));
-%both_pval  = cat_io_csv(fullfile(boneyEvalDir,'HBM_2024','both_pval_Boney.csv'));
-both_corr  = men_corr;
-both_pval  = men_pval; 
-both_corr(2:end,2:end)  = num2cell(mean(cell2mat(cat(3,men_corr(2:end,2:end),women_corr(2:end,2:end))),3));
-both_pval(2:end,2:end)  = num2cell(mean(cell2mat(cat(3,men_pval(2:end,2:end),women_pval(2:end,2:end))),3));
+both_corr  = cat_io_csv(fullfile(boneyEvalDir,'HBM_2024','boney_correlation_proper.csv')); both_corr(2,:) = []; both_corr(:,2) = [];
+both_pval  = cat_io_csv(fullfile(boneyEvalDir,'HBM_2024','boney_cor_pval.csv'));
+%both_corr  = men_corr; both_corr(2:end,2:end)  = num2cell(mean(cell2mat(cat(3,men_corr(2:end,2:end),women_corr(2:end,2:end))),3));
+%both_pval  = men_pval; both_pval(2:end,2:end)  = num2cell(mean(cell2mat(cat(3,men_pval(2:end,2:end),women_pval(2:end,2:end))),3));
 
 
 % we want to create multipe talbes with specific focus, i.e. to evaluate 
 % the bone and then fat measures separatly 
-use_vals   = { % x y
-  [ 9 10 11 12  ,  2:5 , 1 ]  [ 6 8 7 , 9 10 11 12  ,  2:5 ];   % bone
-  [ 9 10 11 12  ,  2:5 , 1 ]  [ 6 8 7 , 9 10 11 12  ,  2:5 ];   % fat
+use_vals   = { 
+  [ 9 10 12  ,  2:5 , 1 13         ]  [ 6 7 8   , 9 11 12  ,  2:5 ] 'bone' [3 7 12]  [3 7];   % bone [BMD, brain, age]
+  [ 2:5 , 1 15:21                  ]  [ 14      ,                2:5 ] 'fat'  [3     ]  [3  ];    % fat  [
+  [ 9 10 12  ,  2:5 , 1 13 , 15:21 ]  [ 6 7 8 14, 9 11 12  ,  2:5 ] 'bone' [3 7 9 14]  [3 4 8];   % bone [BMD, brain, age]
 };
 
 
-for uvi = 1:size(use_vals,1)
+%%
+for uvi = 3 %1:size(use_vals,1)
   %% prepare smaller tables
   men_corr2   = men_corr(   [1 1+use_vals{uvi,1}] , [1 1+use_vals{uvi,2}])'; 
   men_pval2   = men_pval(   [1 1+use_vals{uvi,1}] , [1 1+use_vals{uvi,2}])'; 
@@ -41,59 +41,89 @@ for uvi = 1:size(use_vals,1)
   both_corr2  = both_corr(  [1 1+use_vals{uvi,1}] , [1 1+use_vals{uvi,2}])'; 
   both_pval2  = both_pval(  [1 1+use_vals{uvi,1}] , [1 1+use_vals{uvi,2}])'; 
 
-  % matrix
+  % create figure
   fg = 11; fgh = figure(fg); clf(fg); fgh.Color = [1 1 1]; 
-  fgh.Position(3:4) = [800 400]; 
-  ha = annotation('textbox',[0.005 0.92 0.99 0.08],'string','hi'); %sprintf( ...
-    %'(%d) %s - site %d, N=%d (r~color,p~transparency<%0.03f)',matonly,matonlyn{matonly+1},si, numel(age), pv));
+  fgh.Position(3:4) = [700 + 20*size( men_corr2 ,1 )   100 + 20*size( men_corr2 ,1 ) ]; 
+
+  % title 
+  ha = annotation('textbox',[0.005 0.92 0.99 0.08],'string',...
+    sprintf(['Correlation plot of %s measures with colorcoded r-values ' ...
+    'and transparency for p-values).'], use_vals{uvi,3}));
   ha.FontSize             = fontsize * 1.2;
   ha.FontWeight           = 'bold';
   ha.HorizontalAlignment  = 'center';
   ha.EdgeColor            = 'none'; 
   
-  subplot('Position',[0.24 0.35 0.22 .5]); 
-  im = imagesc( cell2mat(both_corr2(2:end,2:end)) ); 
-  im.AlphaData = min(1,max(0,.1 * log10(1 ./ cell2mat(both_pval2(2:end,2:end) )))); 
-  ax = gca; ax.TickLength = [0 0];
-  ax.XTick = 1:numel( both_corr2(:,1) ); 
-  ax.XTickLabel = cat_io_strrep( both_corr2(1,2:end) ,'_','\_');
-  ax.XTickLabelRotation = 90;
-  ax.YTick = 1:numel( both_corr2(:,1) ); 
-  ax.YTickLabel = cat_io_strrep( both_corr2(2:end,1) ,'_','\_');
-  ax.FontSize = fontsize; 
-  clim([-1 1]);
-  title('both')
+
+  % plot for both sexes - print a matix a a specific position 
+  subplot('Position', [0.20 0.35 0.239 .5]); hold on
+  im                      = imagesc( cell2mat(both_corr2(2:end,2:end)) ); 
+  im.AlphaData            = min(1,max(0,.1 * log10(1 ./ cell2mat(both_pval2(2:end,2:end) )))); 
+  for uvix = use_vals{uvi,4}, plot([uvix uvix]+.5,[1 size(both_corr2,1)]-.5,'Color',[0 0 0]); end
+  for uvix = use_vals{uvi,5}, plot([1 size(both_corr2,2)]-.5,[uvix uvix]+.5,'Color',[0 0 0]); end
+  xlim([0.5 size(both_corr2,2)-.5]); ylim([0.5 size(both_corr2,1)-.5]); clim([-1 1]);
+  % no we have to update the axis labels 
+  ax                      = gca; 
+  ax.TickLength           = [0 0];
+  ax.XTick                = 1:numel( both_corr2(1,2:end) ); 
+  ax.XTickLabel           = cat_io_strrep( both_corr2(1,2:end) ,'_','\_');
+  ax.XTickLabelRotation   = 90;
+  ax.YTick                = 1:numel( both_corr2(2:end,1) ); 
+  ax.YTickLabel           = cat_io_strrep( both_corr2(2:end,1) ,'_','\_');
+  ax.FontSize             = fontsize; 
+  title('both sexes','FontSize',fontsize); box on
   
-  subplot('Position',[0.46 0.35 0.22 .5]); 
-  im = imagesc( cell2mat(women_corr2(2:end,2:end)) ); 
-  im.AlphaData = min(1,max(0,.1 * log10(1 ./ cell2mat(women_pval2(2:end,2:end))) )); 
+
+  % women
+  subplot('Position', [0.44 0.35 0.239 .5]); hold on
+  im                      = imagesc( cell2mat(women_corr2(2:end,2:end)) ); 
+  im.AlphaData            = min(1,max(0,.1 * log10(1 ./ cell2mat(women_pval2(2:end,2:end))) )); 
+  for uvix = use_vals{uvi,4}, plot([uvix uvix]+.5,[1 size(both_corr2,1)]-.5,'Color',[0 0 0]); end
+  for uvix = use_vals{uvi,5}, plot([1 size(both_corr2,2)]-.5,[uvix uvix]+.5,'Color',[0 0 0]); end
+  xlim([0.5 size(both_corr2,2)-.5]); ylim([0.5 size(both_corr2,1)-.5]); clim([-1 1]);
   ax = gca; ax.TickLength = [0 0];
-  ax.XTick = 1:numel( women_corr2(:,1) ); 
-  ax.XTickLabel = cat_io_strrep( both_corr2(1,2:end) ,'_','\_');
-  ax.XTickLabelRotation = 90;
-  ax.YTick = []; 
-  ax.FontSize = fontsize; 
-  clim([-1 1]);
-  title('women')
+  ax.XTick                = 1:numel( both_corr2(1,2:end) ); 
+  ax.XTickLabel           = cat_io_strrep( both_corr2(1,2:end) ,'_','\_');
+  ax.XTickLabelRotation   = 90;
+  ax.YTick                = []; 
+  ax.FontSize             = fontsize; 
+  title('women'); box on
   
-  subplot('Position',[0.68 0.35 0.22 .5]); 
-  im = imagesc( cell2mat(men_corr2(2:end,2:end)) ); 
-  im.AlphaData = min(1,max(0,.1 * log10(1./ cell2mat(men_pval2(2:end,2:end))) )); 
-  ax = gca; ax.TickLength = [0 0];
-  ax.XTick = 1:numel( women_corr2(:,1) ); 
-  ax.XTickLabel = cat_io_strrep( both_corr2(1,2:end) ,'_','\_');
-  ax.XTickLabelRotation = 90;
-  ax.YTick = []; 
-  ax.FontSize = fontsize; 
-  clim([-1 1]);
-  title('men')
+
+  % men 
+  subplot('Position', [0.68 0.35 0.239 .5]); hold on
+  im                      = imagesc( cell2mat(men_corr2(2:end,2:end)) ); 
+  im.AlphaData            = min(1,max(0,.1 * log10(1./ cell2mat(men_pval2(2:end,2:end))) )); 
+  for uvix = use_vals{uvi,4}, plot([uvix uvix]+.5,[1 size(both_corr2,1)]-.5,'Color',[0 0 0]); end
+  for uvix = use_vals{uvi,5}, plot([1 size(both_corr2,2)]-.5,[uvix uvix]+.5,'Color',[0 0 0]); end
+  xlim([0.5 size(both_corr2,2)-.5]); ylim([0.5 size(both_corr2,1)-.5]); clim([-1 1]);
+  ax                      = gca; 
+  ax.TickLength           = [0 0];
+  ax.XTick                = 1:numel( both_corr2(1,2:end) ); 
+  ax.XTickLabel           = cat_io_strrep( both_corr2(1,2:end) ,'_','\_');
+  ax.XTickLabelRotation   = 90;
+  ax.YTick                = []; 
+  ax.FontSize             = fontsize; 
+  %ax.LineWidth            = 2; 
+  title('men'); box on
+
 
   colormap(jet); 
-  subplot('Position',[0.92,0.35,0.0001,0.5]); imagesc( (-1:.1:1)' ); axis equal off
-  cb = colorbar; 
+  subplot('Position',[0.92,0.35,0.1,0.5]); 
+  im            = imagesc( repmat( flip( (-1:.1:1)' ), 1 , 5) );
+  im.AlphaData  = repmat( 0:.25:1 , 21 , 1);   
+  ax            = gca; 
+
+
+%  cb = colorbar; cb.FontSize = fontsize * .9;
+  %cb.Ticks = [-1:0.2:1];
   
-  subplot('Position',[0.96,0.35,0.0001,0.5]); axis equal off
-  cb = colorbar;
+%  subplot('Position',[0.96,0.35,0.1,0.5]); 
+%  im = imagesc( ones(1,11) ); im.AlphaData = 0:0.1:1; clim([0 1]);
+  %cb = colorbar; cb.FontSize = fontsize * .9; 
+  %cb.Ticks = 0:0.25:1; %cb.Label = {0:0.25:1};
+%  axis equal off
+  %cb = colorbar;
   
   %subplot('Position',[0.95,0.35,0.0001,0.5]); 
   %subplot('Position',[0.98,0.35,0.0001,0.5]); imagesc( ones(1,21) ); axis equal off
